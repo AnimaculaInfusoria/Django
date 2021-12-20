fc34406a442d4a4c62cdeec6ac30ed46befdfd59
Kubo Lehotsky <kubolehotsky@gmail.com>
2021-12-20 10:03:17 +0100

    cleaner code + search

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from wagtail.search.models import Query
+from wagtail.core.models import Page
 
-# Create your views here.
+
+def search(request):
+   
+    search_query = request.GET.get('query', None)
+    if search_query:
+        search_results = Page.objects.live().search(search_query)
+
+        
+        Query.get(search_query).add_hit()
+    else:
+        search_results = Page.objects.none()
+
+    
+    return render(request, 'search_results.html', {
+        'search_query': search_query,
+        'search_results': search_results,
+    })
